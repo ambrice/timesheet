b8065158f9cb1b430a21e59353dced4f86c6b169
Aaron Brice <aaron.brice@gmail.com>
2009-05-06 22:50:58 -0700

    Initial empty files generated by android create project

---
+++ src/com/aaronbrice/timesheet/TimesheetActivity.java
@@ -0,0 +1,15 @@
+package com.aaronbrice.timesheet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TimesheetActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ tests/src/com/aaronbrice/timesheet/TimesheetActivityTest.java
@@ -0,0 +1,21 @@
+package com.aaronbrice.timesheet;
+
+import android.test.ActivityInstrumentationTestCase;
+
+/**
+ * This is a simple framework for a test of an Application.  See
+ * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
+ * how to write and extend Application tests.
+ * <p/>
+ * To run this test, you can type:
+ * adb shell am instrument -w \
+ * -e class com.aaronbrice.timesheet.TimesheetActivityTest \
+ * com.aaronbrice.timesheet.tests/android.test.InstrumentationTestRunner
+ */
+public class TimesheetActivityTest extends ActivityInstrumentationTestCase<TimesheetActivity> {
+
+    public TimesheetActivityTest() {
+        super("com.aaronbrice.timesheet", TimesheetActivity.class);
+    }
+
+}
